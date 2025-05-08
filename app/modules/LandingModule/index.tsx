3f8a83dfaef675cba0fbcc639119f6fb55252ea3
TrayMachi <tristan.agra@ui.ac.id>
2025-05-08 15:07:22 +0700

    feat: landing page skeleton

---
+++ app/modules/LandingModule/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingModule = () => {
+  return (
+    <main>_page._index</main>
+  )
+}
--- app/routes/_index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="text-2xl font-semibold">Welcome to Udehnih!</h1>
-    </main>
-  );
-}
