c65897e39f4935a2d24a4981d52c39915912e851
TrayMachi <tristan.agra@ui.ac.id>
2025-05-22 18:33:04 +0700

    feat: redirect to login instead

--- app/routes/api.logout.tsx
+++ app/routes/api.logout.tsx
@@ -2,7 +2,7 @@ import { redirect, type ActionFunctionArgs } from 'react-router';
 import { sessionCookie } from '~/lib/auth.server';
 
 export async function action(args: ActionFunctionArgs) {
-  return redirect('/', {
+  return redirect('/login', {
     headers: {
       'Set-Cookie': await sessionCookie.serialize('', { maxAge: 0 }),
     },
