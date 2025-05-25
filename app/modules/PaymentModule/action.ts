bc226dadc40e261769e191e7d99a724eef1f08a1
akhyarrasyid <akhyarrasyid2005@gmail.com>
2025-05-25 10:40:29 +0700

    feat: payment skeleton

---
+++ app/modules/PaymentModule/action.ts
@@ -0,0 +1,3 @@
+import type { ActionFunctionArgs } from "react-router";
+
+export async function PaymentAction({ request }: ActionFunctionArgs) {}
---
+++ app/modules/PaymentModule/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PaymentModule = () => {
+  return (
+    <main>payment</main>
+  )
+}
